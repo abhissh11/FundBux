e4ac6d06182464e40e9b7dd40c751c01cbf9acf3
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-07-08 15:50:38 +0530

    Implemented About in HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import CampaignsCarousel from "../components/CampaignsCarousel";
+import AboutComponent from "../components/AboutComponent";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
       <CampaignsCarousel />
+      <AboutComponent />
     </div>
   );
 }
