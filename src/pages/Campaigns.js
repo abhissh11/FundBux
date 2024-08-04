7e418ed0da91356e67e01e5289593cee8f51fbe8
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-08-04 13:16:37 +0530

    temp comp

--- src/pages/Campaigns.js
+++ src/pages/Campaigns.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Comp from "../components/Comp";
 
 export default function Campaigns() {
   return (
@@ -17,6 +18,7 @@ export default function Campaigns() {
           help.
         </p>
       </div>
+      <Comp />
     </div>
   );
 }
